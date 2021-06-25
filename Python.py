0e9763305be83d41426539b3049051dfb339f1fc
Huzaifa Muhammad Siddique <53409269+HuzaifaMS@users.noreply.github.com>
2021-06-25 19:07:59 +0500

    Create Python.py

---
+++ Python.py
@@ -0,0 +1,31 @@
+'''
+Python is one of the most popular Programming Language. You can do everything using Python. Either it's Data Science, Machine Learning, AI, Web Development, Game Development,
+Software Development, Writing Complex Programs, etc.
+This guide covers the following things:
+1. Data Types
+2. Variables
+3. Operators
+4. Order of Operations
+5. The 'is' and 'in' keywords
+6. The 'continue' and 'break' keywords
+Logical Operators
+3. Numbers
+4. Python Key words
+4. Strings
+11. Important In-Built Functions
+
+5. Lists
+6. Tuples
+7. Dictionaries
+8. Sets
+9. Conditional Statements
+10. Loops
+Comments
+Try & Except
+Files
+
+
+11. Functions
+12. Classes & Objects
+Classes & Inheritance
+'''
